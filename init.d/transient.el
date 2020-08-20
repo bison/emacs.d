0de7625d2b26792c77826686d4855e2579f82b91
Brad Ison <bison@xvdf.io>
2020-08-20 18:10:21 +0200

    transient: Don't save history on exit

--- init.d/transient.el
+++ init.d/transient.el
@@ -1,5 +1,6 @@
 (use-package transient
   :custom
+  (transient-save-history nil)
   (transient-levels-file (bison/expand-cache-file "transient/levels.el"))
   (transient-values-file (bison/expand-cache-file "transient/values.el"))
   (transient-history-file (bison/expand-cache-file "transient/history.el")))
