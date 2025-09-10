c33793c8857df37d230c895fc5270760ce68038d
Brad Ison <bison@xvdf.io>
2025-09-10 14:56:51 +0200

    Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,16 @@
+;;; early-init.el --- My Emacs configuration  -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2020 Brad Ison
+;;
+;; Author: Brad Ison <bison@xvdf.io>
+;; URL: http://github.com/bison/emacs.d
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+
+;;; Code:
+
+(setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
